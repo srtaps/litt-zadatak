e1fd9176503981de80aa90d2418e1ae3a4dcc490
srt03 <139922324+srtaps@users.noreply.github.com>
2025-03-23 19:38:24 +0100

    Add popup form

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ const mainHeader = document.getElementById("header");
 const toggleMenuButton = document.getElementById("open-menu");
 const hamburgerIcon = document.querySelector("#open-menu svg");
 const closeIcon = document.querySelector("#open-menu p");
+const openFormButton = document.getElementById("form-button");
+const popupForm = document.getElementById("form-popup");
 
 toggleMenuButton.addEventListener("click", (e) => {
   e.preventDefault();
@@ -9,4 +11,10 @@ toggleMenuButton.addEventListener("click", (e) => {
   mainHeader.classList.toggle("active-menu");
   hamburgerIcon.classList.toggle("hide");
   closeIcon.classList.toggle("hide");
+});
+
+openFormButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  popupForm.classList.toggle("popup-form--show");
 });
