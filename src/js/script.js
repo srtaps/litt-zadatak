8505984f4fc5cfb7b3c6e703723cd29ddb4656d8
srt03 <139922324+srtaps@users.noreply.github.com>
2025-03-29 00:45:27 +0100

    Add Swiper, finish testimonials section

--- src/js/script.js
+++ src/js/script.js
@@ -59,3 +59,16 @@ if (window.location.pathname.endsWith("marketing.html")) {
     });
   });
 }
+
+const swiper = new Swiper(".swiper", {
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
